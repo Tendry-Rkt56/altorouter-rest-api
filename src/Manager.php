95d545e4ba6cd965500191ebf0cfc08f93e94779
Tendry <tendryzephyrin@gmail.com>
2024-10-28 12:23:27 +0100

    Ajout d'une méthode de récupération d'entité à la classe Manager

---
+++ src/Manager.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Config\DataBase;
+
+class Manager 
+{
+
+     private static $_instance;
+     private static $_db;
+
+     public function get()
+     {
+          if (self::$_instance == null) self::$_instance = new self();
+          return self::$_instance;
+     }
+
+     public function getDb()
+     {
+          if (self::$_db == null) self::$_db = new DataBase(DB_NAME);
+          return self::$_db;
+     }
+
+     public function getEntity(string $table)
+     {
+          return new $table($this->getDb());
+     }
+
+}
